b8b1d6e0264c2406c4ec643b9cfe7ea581ca0040
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-07 20:36:19 -0300

    initial commit

---
+++ note/note.go
@@ -0,0 +1,44 @@
+package note
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Note struct {
+	Title     string
+	Content   string
+	createdAt time.Time
+}
+
+func (n Note) Display() {
+	fmt.Printf("Your note titled %v has the following Content:\n\n%v\n\n", n.Title, n.Content)
+}
+
+func (n Note) Save() error {
+	filename := strings.ReplaceAll(n.Title, " ", "_")
+	filename = strings.ToLower(filename) + ".json"
+
+	jsonData, err := json.Marshal(n)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, jsonData, 0644)
+}
+
+func New(title, content string) (*Note, error) {
+	if title == "" || content == "" {
+		return nil, errors.New("invalid input")
+	}
+
+	return &Note{
+		Title:     title,
+		Content:   content,
+		createdAt: time.Now(),
+	}, nil
+}
